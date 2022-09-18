2dc2fb825694c6307cda9f3b8d46dc33545840e7
eriktlu <36303097+eriktlu@users.noreply.github.com>
2022-09-18 23:51:21 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const main = document.querySelector("main");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const menu_1 = document.querySelectorAll(".nav-list li a")[0];
+const menu_2 = document.querySelectorAll(".nav-list li a")[1];
+
+let elementsArray = [navToggle, menu_1, menu_2]
+
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("click", () => {
+        navToggle.setAttribute("aria-expanded", !primaryNav.hasAttribute("data-visible"));
+        primaryNav.toggleAttribute("data-visible");
+        main.toggleAttribute("data-overlay");
+    })
+})
+
+main.addEventListener("click", () => {
+    if(main.hasAttribute("data-overlay")){
+        navToggle.setAttribute("aria-expanded", !primaryNav.hasAttribute("data-visible"));
+        primaryNav.toggleAttribute("data-visible");
+        main.toggleAttribute("data-overlay");
+    }
+})
+
+// function scrollToElement (id) {
+//     var el = document.querySelector('#' + id);
+//     window.scrollTo(el.offset().top-50);
+//     console.log(el.offset().top)
+// }
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: false,
+    dots: true,
+    arrows: false
+});
